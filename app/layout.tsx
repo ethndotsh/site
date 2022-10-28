4c805341c57a2838a221dd2a24883d0d61fe2d44
curse <58047023+cursecodes@users.noreply.github.com>
2022-10-27 21:16:49 -0600

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { AnalyticsWrapper } from "./components/analytics";
+
 import "./globals.css";
 import "./krona.css";
 import "./inter.css";
@@ -17,7 +19,10 @@ export default function RootLayout({
           content="Canadian high school student creating masterpieces with the most bleeding-edge tools. Full-stack developer primarily focused on TypeScript, Next ▲ and Golang."
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   );
 }
